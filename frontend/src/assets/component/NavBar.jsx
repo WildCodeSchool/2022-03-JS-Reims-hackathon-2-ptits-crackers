5f23b873dc03dd0974f5aea127d6d3aa1343b3d2
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-29 16:21:41 +0200

    Start structure of the navbar

---
+++ frontend/src/assets/component/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <img src="" alt="" />
+      <ul>
+        <li>Board</li>
+        <li>Mon agence</li>
+        <li>Mon agenda</li>
+        <li>Mes projets</li>
+      </ul>
+    </nav>
+  );
+}
