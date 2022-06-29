d7019d71ab6f7a4c5b3865730d33eb09faea4d44
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-30 00:21:54 +0200

    first version of the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,38 @@
+import { Link } from "react-router-dom";
+import whiteLogo from "../assets/images/apside-logo.png";
+
 export default function Navbar() {
   return (
     <nav className="block w-[240px] h-screen bg-[#183650]">
-      <p className="white text-center">Hello user ! </p>
-      <img src="" alt="user" className="rounded-full" />
-      <ul className="">
-        <li className="li-navbar">Board</li>
-        <li className="li-navbar">Mon agence</li>
-        <li className="li-navbar">Mon agenda</li>
-        <li className="li-navbar">Mes projets</li>
-      </ul>
+      <div className="flex flex-col justify-evenly">
+        <p className="font-['Hind-Regular'] text-[1.5rem] font-medium p-[1.25rem] text-slate-50 text-center">
+          Bonjour !
+        </p>
+        <img
+          src="https://pixabay.com/photos/woman-business-fashion-young-3060784/"
+          alt="user"
+          className="rounded-full mt-8 mb-8"
+        />
+        <ul className="flex flex-col items-center">
+          <li className="li-navbar">
+            <Link to="/">Board</Link>
+          </li>
+          <li className="li-navbar">
+            <Link to="/myagency">Mon agence</Link>
+          </li>
+          <li className="li-navbar">
+            <Link to="/agenda">Mon agenda</Link>
+          </li>
+          <li className="li-navbar mb-20">
+            <Link to="/myprojects">Mes projets</Link>
+          </li>
+        </ul>
+        <img
+          src={whiteLogo}
+          alt="logo blanc"
+          className="max-w-[75%] justify-self-end self-center mb-8"
+        />
+      </div>
     </nav>
   );
 }
