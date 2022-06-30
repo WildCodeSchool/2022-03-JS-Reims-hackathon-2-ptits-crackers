083faaacc4b11acd7bd216604520f3fc595d5c5a
PiXeleum <jeffreyrigaux@gmail.com>
2022-06-30 10:06:01 +0200

    add town in menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
-
+import Search from "./pages/Search";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Search />
     </div>
   );
 }
