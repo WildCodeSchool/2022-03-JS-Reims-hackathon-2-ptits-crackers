de21572e1f4dd622dae266cc6326f576e9d44f9c
PiXeleum <jeffreyrigaux@gmail.com>
2022-06-30 16:11:15 +0200

    change calender in user account

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -2,8 +2,8 @@ import { DayPilotCalendar } from "@daypilot/daypilot-lite-react";
 
 export default function Calendar() {
   return (
-    <div className="w-2/6 m-5">
-      <DayPilotCalendar days={1} scale="Day" />
+    <div className="w-3/6 m-5">
+      <DayPilotCalendar days={7} scale="Day" />
     </div>
   );
 }
