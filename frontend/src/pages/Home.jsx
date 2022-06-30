e15f32358c99c0312a6327226847f9c1cff7f53b
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-30 10:24:27 +0200

    Structure for the post-it"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,11 @@
-import { Link } from "react-router-dom";
 import PostIt from "../components/PostIt";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex">
       <Navbar />
       <PostIt />
-      <h1 className="text-3xl font-bold underline">Hello les crackers</h1>
-      <p>
-        Voir <Link to="/user">users</Link>
-      </p>
-      <p>
-        Voir <Link to="/project">projets</Link>
-      </p>
-    </>
+    </main>
   );
 }
