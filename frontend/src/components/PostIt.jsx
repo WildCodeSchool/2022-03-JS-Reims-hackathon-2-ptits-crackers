48f89790059dfeb96f2351e73c80f0bc0b9d8323
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-30 14:16:28 +0200

    changes postit

--- frontend/src/components/PostIt.jsx
+++ frontend/src/components/PostIt.jsx
@@ -6,15 +6,15 @@ export default function Project() {
       <div className="postit">
         <article>
           <h3>EVOLION 3 SAFT</h3>
-          <p className="italic">
+          <p className="italic mb-1">
             Ingénierie industrielle,
             <br />
             agence de Bordeaux.
           </p>
-          <p>
+          <p className="mt-1">
             Saft Batteries a décidé de rénover son système Evolion, contraction
             de « evolution » et « Li-ion », avec une troisième génération plus
-            performante, plus sûre et plus maintenable.
+            performante, plus sûre et plus maintenable.{" "}
           </p>
         </article>
       </div>
